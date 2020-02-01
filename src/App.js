2e66685c9715701b77213225779e9e7e57cb7f02
chunchuney@gmail.com <{chunchuney@gmail.com}>
2020-02-01 17:56:42 +0900

    20200201 - dispatch

--- src/App.js
+++ src/App.js
@@ -22,8 +22,8 @@ export  default App;
 // subscribe : 스토어의 state를 props 매핑
 // const  mapStateToProps = (state) => ({
 //   //왼쪽은 props, 오른쪽은 store의 state
-//   aaa: state.productReducer,
-//   bbb: state.userReducer
+//     product: state.productReducer,
+//     user: state.userReducer
 // });
 //
 // // dispatch : {액션을 디스패치하는} 펑션을 props로 매핑
