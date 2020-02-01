d3e5082da8f3934987a834fdc20a8e46f88218fe
chunchuney@gmail.com <{chunchuney@gmail.com}>
2020-02-01 14:59:39 +0900

    20200201 - reduce

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from "redux";
+
+const reducer = () => {
+    return 'State';
+};
+
+const store = createStore(reducer);
+console.log(store);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
